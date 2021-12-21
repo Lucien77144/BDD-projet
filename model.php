65ecbc5874174d7b6e4b2e389ca79565f998e580
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2021-12-21 16:33:09 +0100

    up DB connect

--- model.php
+++ model.php
@@ -1,6 +1,6 @@
 <?php
 
-// lien avec la base de donnée
+// lien avec la base de donnée (LOCALHOST)
 function connect(){
     $db= new PDO ('mysql:host=localhost;dbname=tpfinal;port3306;charset=utf8','root', '', array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     return $db;
